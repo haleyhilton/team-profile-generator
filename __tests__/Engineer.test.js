928e01458c687790a8b0485315913c1b170b0769
Tater <haleymariehilton@gmail.com>
2022-06-01 14:07:34 -0700

    Updated engineer test and runs / passes

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,30 +1,21 @@
-const Employee = require('../lib/Employee')
+const Engineer = require('../lib/Engineer')
 const testName = "Michael Scott"
 const testEmail = "ms@gmail.com"
 const testID = "1234"
 
 
-test("Employee object created", () => {
-        const e = new Employee(testName, testEmail, testID);
+test("Engineer object created", () => {
+        const engineer = new Engineer("Michael Scott", 45, "ms@gmail.com", "michaelSCOTT1");
+        expect(engineer.github).toEqual(expect.any(String));
+
     });
 
-    test("e.getName will set name", () => {
-        const name = "Michael Scott";
-        const e = new Employee(testName, testEmail, testID);
-        expect(e.getName()).toEqual(testName);
+    test("engineer.getGithub returns and parses string input", () => {
+        const engineer = new Engineer("Michael Scott", 45, "ms@gmail.com", "michaelSCOTT1");
+        expect(engineer.github).toEqual(expect.any(String));
     });
 
-    test("e.getID will return any input number", () => {
-        const e = new Employee(testName, testEmail, testID);
-        expect(e.getId()).toEqual(testID);
-    });
-
-    test("e.getEmail will set email string", () => {
-        const e = new Employee(testName, testEmail, testID);
-        expect(e.getEmail()).toEqual(testEmail);
-    });
-
-    test("e.getRole will assign and return Employee", () => {
-        const e = new Employee(testName, testEmail, testID);
-        expect(e.getRole()).toEqual("Employee");
+    test("engineer.getRole will assign and return Engineer", () => {
+        const engineer = new Engineer("Michael Scott", 45, "ms@gmail.com", "michaelSCOTT1");
+        expect(engineer.getRole()).toEqual("Engineer");
     });
